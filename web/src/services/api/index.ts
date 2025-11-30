6624a5e37e9cdedac6192edbec84a8a93af90cc1
MKhai205 <manhkhai123459876@gmail.com>
2025-11-30 22:54:57 +0700

    feat: add incident service for managing incident reports and statistics

--- web/src/services/api/index.ts
+++ web/src/services/api/index.ts
@@ -3,6 +3,7 @@
  */
 export * from './air-quality.service';
 export * from './alert.service';
+export * from './incident.service';
 export * from './weather.service';
 export * from './stations.service';
 export * from './users.service';
