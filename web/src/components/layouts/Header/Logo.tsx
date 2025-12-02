4db5fe937ed93fe106c7286b85e7b8210c5a9e23
Mkhai205 <manhkhai123459876@gmail.com>
2025-12-03 01:25:16 +0700

    feat: update favicon and logo in layout to new logo image

--- web/src/components/layouts/Header/Logo.tsx
+++ web/src/components/layouts/Header/Logo.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 interface LogoProps {
   className?: string;
 }
@@ -5,9 +7,7 @@ interface LogoProps {
 export function Logo({ className = '' }: LogoProps) {
   return (
     <div className={`flex items-center gap-2 ${className}`}>
-      <div className="w-7 h-7 bg-linear-to-br from-blue-500 to-cyan-500 rounded-lg flex items-center justify-center">
-        <span className="text-white text-sm font-bold">SF</span>
-      </div>
+      <Image src="/logo-1.png" alt="Smart Forecast Logo" width={42} height={32} />
       <h1 className="text-slate-900 text-sm md:text-base font-semibold">Smart Forecast</h1>
     </div>
   );
